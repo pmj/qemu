e5abedfd9a569160a92f6d7a29d22babe7c2f6dc
Alexander Graf <graf@amazon.com>
2023-08-30 16:14:25 +0000

    hw/vmapple/vmapple: Add vmapple machine type
    Apple defines a new "vmapple" machine type as part of its proprietary
    macOS Virtualization.Framework vmm. This machine type is similar to the
    virt one, but with subtle differences in base devices, a few special
    vmapple device additions and a vastly different boot chain.
    
    This patch reimplements this machine type in QEMU. To use it, you
    have to have a readily installed version of macOS for VMApple,
    run on macOS with -accel hvf, pass the Virtualization.Framework
    boot rom (AVPBooter) in via -bios, pass the aux and root volume as pflash
    and pass aux and root volume as virtio drives. In addition, you also
    need to find the machine UUID and pass that as -M vmapple,uuid= parameter:
    
    $ qemu-system-aarch64 -accel hvf -M vmapple,uuid=0x1234 -m 4G \
        -bios /System/Library/Frameworks/Virtualization.framework/Versions/A/Resources/AVPBooter.vmapple2.bin
        -drive file=aux,if=pflash,format=raw \
        -drive file=root,if=pflash,format=raw \
        -drive file=aux,if=none,id=aux,format=raw \
        -device vmapple-virtio-blk-pci,variant=aux,drive=aux \
        -drive file=root,if=none,id=root,format=raw \
        -device vmapple-virtio-blk-pci,variant=root,drive=root
    
    With all these in place, you should be able to see macOS booting
    successfully.
    
    Known issues:
     - Currently only macOS 12 guests are supported. The boot process for
       13+ will need further investigation and adjustment.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>
    Co-authored-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Signed-off-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Reviewed-by: Akihiko Odaki <akihiko.odaki@daynix.com>

---
+++ contrib/vmapple/uuid.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Used for converting a guest provisioned using Virtualization.framework
+# for use with the QEMU 'vmapple' aarch64 machine type.
+#
+# Extracts the Machine UUID from Virtualization.framework VM JSON file.
+# (as produced by 'macosvm', passed as command line argument)
+
+plutil -extract machineId raw "$1" | base64 -d | plutil -extract ECID raw -
+
